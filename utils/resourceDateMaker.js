8971e61a948c6e1f49852d9b28d47b406a8ed59b
Mazharul Emon <emonmazharul@yandex.com>
2021-02-21 00:48:22 +0600

    data error

--- utils/resourceDateMaker.js
+++ utils/resourceDateMaker.js
@@ -3,6 +3,7 @@ function resourceDateMaker () {
 	const yesterDayDate = new Date().getDate() - 1;
 	let yesterDay = new Date().setDate(yesterDayDate);
 	yesterDay = new Date(yesterDay).toLocaleDateString();
+	console.log(today,yesterDay);
 	return {
 		today:today.split('/').reverse().join('-'),
 		yesterDay:yesterDay.split('/').reverse().join('-')
