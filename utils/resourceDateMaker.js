c3012febebc39edc776828e80e417a2428ff6799
Mazharul Emon <emonmazharul@yandex.com>
2021-02-21 01:29:38 +0600

    making the data more and more dynamic

--- utils/resourceDateMaker.js
+++ utils/resourceDateMaker.js
@@ -1,15 +1,17 @@
-function resourceDateMaker () {
-	let today = new Date().toLocaleDateString();
-	const yesterDayDate = new Date().getDate() - 1;
-	let yesterDay = new Date().setDate(yesterDayDate);
-	yesterDay = new Date(yesterDay).toLocaleDateString();
-	const [todayMonth,todayDate,todayYear] = today.split('/');
-	const [yesterDayMonth,yesterDaydate,yesterDayYear] = yesterDay.split('/');
 
+function dateInfoParser(arg) {
+	const [year,month,date] = [ arg.getFullYear() + '' , arg.getMonth() + 1 +'' , arg.getDate() + '' ];
+	return year + '-' + month + '-' + date ;
+}
+
+function resourceDateMaker () {
+	const yesterDayDate = new Date().getDate() - 1;
+	let yesterDay = new Date().setDate(yesterDayDate); 
 	return {
-		today:todayYear+ '-' + todayMonth + '-' +todayDate,
-		yesterDay:yesterDayYear+ '-' + yesterDayMonth+ '-' + yesterDaydate
+		today:dateInfoParser(new Date()),
+		yesterDay:dateInfoParser(new Date(yesterDay)),
 	}
 }
+
 
 module.exports = resourceDateMaker;
